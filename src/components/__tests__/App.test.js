630cf65cf8ef56af4646b25261cec01baa6bf854
michaldobiezynski <dobiezynski@gmail.com>
2020-03-20 12:31:07 +0000

    enzyme renderers

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme'
 
 import App from '../App';
+import CommentBox from '../CommentBox';
 
 it('shows a comment box', () => {
-    const div = document.createElement('div');
+    const wrapped = shallow(<App />);
 
-    ReactDOM.render(<App />, div);
-    
-    //Looks inside the div
-    //and checks to see if the CommentBox is in there
-    
-    expect(div.innerHTML).toContain('Comment Box')
+    expect(wrapped.find(CommentBox).length).toEqual(1);
 
-
-    ReactDOM.unmountComponentAtNode(div); //this is clean up
 });
