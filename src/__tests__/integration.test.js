ea4c8accfa2455003f499f1d13e3f507d0ec1ab4
michaldobiezynski <dobiezynski@gmail.com>
2020-03-25 08:56:33 +0000

    introducing a pause

--- src/__tests__/integration.test.js
+++ src/__tests__/integration.test.js
@@ -6,29 +6,32 @@ import Root from 'Root';
 import App from 'components/App';
 
 beforeEach(() => {
-    moxios.install();
-    moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
-        status: 200,
-        respomse: [{name: 'Fetch #1'}, {name: 'Fetched #2'}]
-    });
+  moxios.install();
+  moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [{ name: 'Fetched #1' }, { name: 'Fetched #2' }]
+  });
 });
 
 afterEach(() => {
-    moxios.uninstall();
+  moxios.uninstall();
 });
 
-it('can fetch a list of comments and display them', () => {
+it('can fetch a list of comments and display them', done => {
+  const wrapped = mount(
+    <Root>
+      <App />
+    </Root>
+  );
 
-    //Attempt to render the *entire* app
-    const wrapped = mount(
-        <Root>
-            <App />
-        </Root>
-    );
+  wrapped.find('.fetch-comments').simulate('click');
 
-    //find the 'fetchComments' button and click it
-    wrapped.find('.fetch-comments').simulate('click');
+  moxios.wait(() => {
+    wrapped.update();
 
-    //Expect to find a list of comments!  
     expect(wrapped.find('li').length).toEqual(2);
+
+    done();
+    wrapped.unmount();
+  });
 });
