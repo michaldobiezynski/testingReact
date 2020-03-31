3eed2e906b626ca623189935e274ee04fe32aa09
michaldobiezynski <dobiezynski@gmail.com>
2020-03-31 09:28:22 +0100

    Adding Routes and Auth Reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 import { SAVE_COMMENT } from 'actions/types';
 import { FETCH_COMMENTS } from 'actions/types';
+import { CHANGE_AUTH } from 'actions/types';
 
 export function saveComment(comment) {
   return {
@@ -16,5 +17,12 @@ export function fetchComments() {
   return {
     type: FETCH_COMMENTS,
     payload: response,
+  };
+}
+
+export function changeAuth(isLoggedIn) {
+  return {
+    type: CHANGE_AUTH,
+    payload: isLoggedIn,
   };
 }
