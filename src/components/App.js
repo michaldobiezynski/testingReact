bdfa0456bb8e8a578a9091cde4025a1fff330fc7
michaldobiezynski <dobiezynski@gmail.com>
2020-04-01 08:38:42 +0100

    wiring up state, rendering header - previous commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
-export default class App extends Component {
-  renderButton() {}
+class App extends Component {
+  renderButton() {
+    if (this.props.auth) {
+      return <button>Sign Out</button>;
+    } else {
+      return <button>Sign In</button>;
+    }
+  }
 
   renderHeader() {
     return (
@@ -16,7 +23,7 @@ export default class App extends Component {
         <li>
           <Link to='/post'>Post A Comment</Link>
         </li>
-        <li>{this.renderButton}</li>
+        <li>{this.renderButton()}</li>
       </ul>
     );
   }
@@ -24,9 +31,16 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        {this.renderHeader()}
         <Route path='/post' component={CommentBox} />
         <Route path='/' exact component={CommentList} />
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(App);
