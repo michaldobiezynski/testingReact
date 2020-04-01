5baec91e6510a97e6ba61ddd3bc7fa0e178487e0
michaldobiezynski <dobiezynski@gmail.com>
2020-04-01 08:27:09 +0100

    wiring up state

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,32 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
-const App = () => {
-  return (
-    <div>
-      <Route path='/post' component={CommentBox} />
-      <Route path='/' exact component={CommentList} />
-    </div>
-  );
-};
+export default class App extends Component {
+  renderButton() {}
 
-export default App;
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/post'>Post A Comment</Link>
+        </li>
+        <li>{this.renderButton}</li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <Route path='/post' component={CommentBox} />
+        <Route path='/' exact component={CommentList} />
+      </div>
+    );
+  }
+}
