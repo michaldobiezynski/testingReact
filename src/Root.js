f8b577005abfe1b3b8e2f77ba90a783f49e61a62
michaldobiezynski <dobiezynski@gmail.com>
2020-04-06 09:23:58 +0100

    generating JSON schema and middleware creation - for validating actions payload

--- src/Root.js
+++ src/Root.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import async from 'middlewares/async';
 
+import async from 'middlewares/async';
+import stateValidator from './middlewares/stateValidator';
 import reducers from 'reducers';
 
 const Root = ({ children, initialState = {} }) => {
-  const store = createStore(reducers, initialState, applyMiddleware(async));
+  const store = createStore(
+    reducers,
+    initialState,
+    applyMiddleware(stateValidator, async),
+  );
 
   return <Provider store={store}>{children}</Provider>;
 };
